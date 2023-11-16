083ede9802f1e37ab8be043e7c5d3c9c179ce48f
Kurt Prado <k.prad7571@gmail.com>
2023-11-16 12:56:48 -0600

    docs: commented function purposes

--- FileSave.py
+++ FileSave.py
@@ -6,18 +6,23 @@ savePath = Path(os.getcwd() + '/saves/')
 saveFile = Path('students.txt')
 
 def CreatePath() -> None:
-    if not(os.path.isdir(savePath)) == True:
+    # creates saves directory for datasheets 
+    if not(os.path.isdir(savePath)) == True: # checks if the directory exists
         print('true')
         os.mkdir(savePath)
         _prevdir = ConnectPath()
-        _stream = open(saveFile, 'w+')
+        _stream = open(saveFile, 'w+') # creates new text file for student grades
         CloseSave(_stream, _prevdir)
     print(os.getcwd())
 
 def ConnectPath() -> None:
-    prevdir = os.getcwd()
+    prevdir = os.getcwd() #
+    '''
+    previous directory saved as variable so to return to after closing function
+    without it, imports will not work in Prog
+    '''
     os.chdir(savePath)
-    return prevdir
+    return prevdir # previous directory returned
 
 def OpenSave():
     prevdir = ConnectPath()
@@ -27,17 +32,17 @@ def OpenSave():
         return open(saveFile, 'w+'), prevdir
 def CloseSave(stream, dir) -> None:
     stream.close()
-    os.chdir(dir)
+    os.chdir(dir) #changes directory to pass Path, this case the previous directory
 
 def LoadData(students) -> None:
     save, prevdir = OpenSave()
-    students.clear()
+    students.clear() # deletes all previous dictionaries to remove chance of duplicates
     for line in save.readlines():
-        students.append(json.loads(line))
+        students.append(json.loads(line)) # json.loads() converts line from text file to python dictionary
     CloseSave(save, prevdir)
 
 def SaveData(students) -> None:
     save, prevdir = OpenSave()
     for student in students:
-        save.write(json.dumps(student)+'\n')
+        save.write(json.dumps(student)+'\n') # json.dumps() converts python dictionary to text file
     CloseSave(save, prevdir)
